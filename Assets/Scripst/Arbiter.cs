a1dbd3c7b07ef2626477c655b08b42f194a44d11
MarcoAnt9 <acalde27@hotmail.com>
2021-06-12 18:37:19 -0500

    Linked Arbiter GameObject with the players GameObjects, made code a little bit prettier.

--- Assets/Scripst/Arbiter.cs
+++ Assets/Scripst/Arbiter.cs
@@ -2,24 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Arbiter : MonoBehaviour
 {
-    private PlayerBase pj1 = new PlayerBase();
-    private PlayerBase pj2;
-    private PlayerBase pj3;
-    private PlayerBase pj4;
+    // Players
+    public GameObject pj1;
+    public GameObject pj2;
+    public GameObject pj3;
+    public GameObject pj4;
+
+    private PlayerBase p1Script;
+    private PlayerBase p2Script;
+    private PlayerBase p3Script;
+    private PlayerBase p4Script;
+
+    // Reference Matrix
+    public float[,] repMatrix = new float[4, 4]
+        {
+        {0.5f, 0.8f, 0.5f, 0.2f},
+        {0.8f, 0.2f, 0.3f, 0.4f},
+        {0.5f, 0.3f, 0.6f, 0.8f},
+        {0.2f, 0.4f, 0.8f, 0.9f}
+        };
+
+    public string playerName = "Default Player Name";
+    public string pKingdomName = "Default Player Kingdom Name";
+
+    private int playerPlaying = 0;
+
+    private string p2kName;
+    private string player2Name;
+    private string p3kName;
+    private string player3Name;
+    private string p4kName;
+    private string player4Name;
+
+
+
+    private string[] pNames = new string[] { "name1", "name2", "name3", "name4", "name5" };
+    private string[] kNames = new string[] { "kingdom1", "kingdom2", "kingdom3", "kingdom4", "kingdom5" };
 
     // Start is called before the first frame update
     void Start()
     {
+        p2kName = pNames[1];
+        player2Name = kNames[1];
+
+        p3kName = pNames[2];
+        player3Name = kNames[2];
+
+        p4kName = pNames[3];
+        player4Name = kNames[3];
+
+        initializeClasses(p1Script, p2Script, p3Script, p4Script);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (p1Script.actions == 0 || p1Script.choosing == false)
+        {
+            p2Script.actions = 3;
+            p2Script.choosing = true;
+        }
     }
 
+    public void initializeClasses(PlayerBase p1Script, PlayerBase p2Script, PlayerBase p3Script, PlayerBase p4Script)
+    {
+        // PlayerBase Arguments:
+        // id: 0 - 4 (0 = Human Player) | kName: String | pName: String
+        // pType: Bool (true = Player, default = false) | army: int (default = 0)
+        // civilians: int (default = 0) | money: int (default = 0) | nHeroes: int (default = 0)
+        // prod: int (default = 0)
+
+        // Player 1 - Human
+        //pj1 = new PlayerBase(id: 0, kName: pKingdomName, pName: playerName,
+        //                    pType: true, army: 30, civilians: 300, money: 500, prod: 0);
+        p1Script = pj1.GetComponent<PlayerBase>();
+        p1Script.pType = true;
+        p1Script.actions = 3;
+        p1Script.initializeHP();
+
+        // Player 2 - CPU
+        //pj2 = new PlayerBase(id: 1, kName: p2kName, pName: player2Name, army: 10,
+        //                    civilians: 600, money: 400, prod: 0);
+        //pj2.initializeHP();
+        p2Script = pj2.GetComponent<PlayerBase>();
+        p2Script.pName = player2Name;
+        p2Script.kName = p2kName;
+        p2Script.initializeHP();
+
+        // Player 3 - CPU
+        //pj3 = new PlayerBase(id: 2, kName: p3kName, pName: player3Name, army: 30,
+        //                     civilians: 400, money: 500, nHeroes: 1, prod: 0);
+        //pj3.initializeHP();
+        p3Script = pj3.GetComponent<PlayerBase>();
+        p3Script.pName = player3Name;
+        p3Script.kName = p3kName;
+        p3Script.initializeHP();
+
+        // Player 4 - CPU
+        //pj4 = new PlayerBase(id: 3, kName: p4kName, pName: player4Name, army: 50,
+        //                     civilians: 300, money: 400, nHeroes: 2, prod: 0);
+        //pj4.initializeHP();
+        p4Script = pj4.GetComponent<PlayerBase>();
+        p4Script.pName = player4Name;
+        p4Script.kName = p4kName;
+        p4Script.initializeHP();
+    }
+
+    void changePlayer()
+    {
+        playerPlaying++;
+        if (playerPlaying == 3)
+        {
+            playerPlaying = 0;
+        }
+    }
 
 }
--- Assets/Scripst/PlayerBase.cs
+++ Assets/Scripst/PlayerBase.cs
@@ -2,8 +2,11 @@
 
 public class PlayerBase : MonoBehaviour
 {
+    public GameObject arbiter;
     public bool choosing = false;
+    public bool playing = true;
     public bool pType; // true = player - false = npc
+    public int actions = 0; // Number of actions given by the arbiter to each player.
     public int army;
     public int baseHp = 1000;
     public int civilians;
@@ -12,11 +15,20 @@ public class PlayerBase : MonoBehaviour
     public int maxHp;
     public int money;
     public int nHeroes = 0;
-    public int prod; // produccion del reino
-    public string kName; // nombre del reino
+    public int prod; // Produccion del reino
+    public string kName; // Nombre del reino
+    public string pName; //Nombre del jugador
+
+
+    public float[,] repMax = GameObject.Find("Arbiter").GetComponent<float[,]>();
+    private int numOpions = 8; // Options for the CPU player.
+    private float ownRep;
+
 
     // Class constructor
-    public PlayerBase(int id, bool pType = false, int army = 0, int civilians = 0, int money = 0, int nHeroes = 0, int prod = 0, string kName = "Default")
+    public PlayerBase(int id, string kName, string pName, bool pType = false,
+                      int army = 0, int civilians = 0, int money = 0, int nHeroes = 0,
+                      int prod = 0)
     {
         this.pType = pType;
         this.army = army;
@@ -26,54 +38,139 @@ public class PlayerBase : MonoBehaviour
         this.nHeroes = nHeroes;
         this.prod = prod;
         this.kName = kName;
+        this.pName = pName;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        this.maxHp = baseHp + nHeroes * 250 + army * 50 + civilians;
-        this.currentHp = this.maxHp;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        ownRep = repMax[this.id, this.id];
+        if (pType == false)
+        {
+            while (actions != 0)
+            {
+                int randint = Random.Range(0, numOpions);
 
+                switch (randint)
+                {
+                    case 0:
+                        recruitSoldiers(repMax[this.id, this.id], 10);
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
     }
 
-    void recruitSoldiers(float repK)
+    /// <summary>
+    /// Method to increase Military Strengh, reduces population (and production by proxy), money 
+    /// and reputation with other countries.
+    /// </summary>
+    /// <param name="repK">Internal reputation, affects purchace prices</param>
+    /// <param name="numSoldiers">Number of soldiers to recruit</param>
+    void recruitSoldiers(float repK, int numSoldiers) // 1
     {
-        // - disminuir civiles
-        // - disminuir recursos (multiplicador)
-        // - disminuir relaciones con otros paises
+        // Check if player has the money to complete action
+        int moneyReduction = (int)(this.money - 100 * repK);
+        if (moneyReduction > 0)
+        {
+            // - Decrease Civilians
+            this.civilians -= numSoldiers * 10;
+            // - Alter Production Rate
+            this.prod = this.civilians / 2;
+            // - Reduce Resources
+            this.money -= moneyReduction;
+            // - Lower Relations with other countries
+            affectRelations(repMax, this.id, -0.2f);
+            // - Reduce number of actions
+            reduceActions();
+        }
+        // No money
+        else
+        {
+            Debug.Log("Action cannot be completed");
+        }
     }
 
-    void increasePopulation(float repK)
+    /// <summary>
+    /// Action to increase population, increases production rates and reduces money.
+    /// </summary>
+    /// <param name="repK"></param>
+    /// <param name="numCivil"></param>
+    void increasePopulation(float repK, int numCivil) // 2
     {
-        // aumentar civiles
-        // disminuye money
-        // aumenta prod
+        // Check if player has the money to complete action
+        int moneyReduction = (int)(this.money - 50 * repK);
+        if (moneyReduction > 0)
+        {
+            // Increase number of civilians
+            this.civilians += numCivil;
+            // Affect Production rates
+            this.prod = civilians / 2;
+            // Reduce money
+            this.money -= moneyReduction;
+            // Reduce number of actions
+            reduceActions();
+        }
+        // No money
+        else
+        {
+            Debug.Log("Action cannot be completed");
+        }
     }
 
-    void increaseRep(int id, int lvl)
+    /// <summary>
+    /// Action to increase Reputation with other countries (can target itself), reduces money.
+    /// </summary>
+    /// <param name="ownId">"Sender" id to match in the Reputation Matrix.</param>
+    /// <param name="targetId">"Target" id to match in the Reputation Matrix.</param>
+    /// <param name="lvl">Increase tier, affects money.</param>
+    void increaseRep(int ownId, int targetId, int lvl) // 3
     {
-        // aumenta rep del pais seleccionado (se puede seleccionar a si mismo)
+        int moneyReduction = this.money - 100 * lvl;
+        //affectRelations()
+        // Increases reputation with target country (can select oneself)
+
         // disminuye recurso con base en el lvl seleccionado (lvl actua como multiplicador)
     }
 
-    void collectResourses(int civilSent)
+    void collectResourses(int civilSent) // 4
     {
         // aumenta money (inmediato)
         // reduccion gradual de la poblacion enviada a la recolectar
         // reduccion de repK (propia)
     }
 
-    void endTurn()
+    void endTurn() // 5
     {
         // finaliza el turno
+        actions = 0;
+        choosing = false;
     }
 
-    void sabotage(int idTarget, int type, float repK)
+    void sabotage(int idTarget, int type, float repK) // 6
     {
         // aumenta el tipo de recuso seleccionado
         // disminye el recurso seleccionado del objetivo
@@ -87,13 +184,15 @@ public class PlayerBase : MonoBehaviour
         // envia senial al jugador de que perdio
     }
 
-    void attack(int idTarget)
+    void attack(int idTarget) // 7
     {
         // le informa al arbitro de la intencion del ataque
         // envia this.id y idTarget
+
+        //check health
     }
 
-    void recruitHero()
+    void recruitHero() // 8
     {
         // aqui se reclutan heroes (+1)
         // disminuye money (++)
@@ -105,6 +204,38 @@ public class PlayerBase : MonoBehaviour
         if (this.money < 0)
         {
             this.money = 0;
+        }
+    }
+
+    public void initializeHP()
+    {
+        this.maxHp = baseHp + nHeroes * 250 + army * 50 + civilians;
+        this.currentHp = this.maxHp;
+        this.prod = civilians / 2;
+        Debug.Log($"Player {this.id + 1}:\n Player Name: { this.pName}\n Player Kingdom Name: { this.kName}\n Player HP: {this.currentHp}");
+    }
+
+    void reduceActions()
+    {
+        this.actions--;
+    }
+
+    void affectRelations(float[,] matrix, int ownId, float effect, int targetId = -1)
+    {
+        if (targetId == -1)
+        {
+            int i;
+            for (i = 0; i < 4; i++)
+            {
+                if (i != ownId)
+                {
+                    matrix[ownId, i] += effect;
+                }
+            }
+        }
+        else
+        {
+            matrix[ownId, targetId] += effect;
         }
     }
 
