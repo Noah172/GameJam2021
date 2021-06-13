5493d063c902cea45077a1e310dc239cb2ca824d
Noah172 <jsebastian.garcia1995@gmail.com>
2021-06-13 02:43:00 -0500

    update
    Added: script to the actions of the buttons

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public Button Action;
+    public Button Character;
+    public Button Stats;
+    public Button Options;
+    public Button Skip;
+
+    public Canvas Option;
+    public Canvas Actions;
+    public Canvas Info;
+    public Canvas Stat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void clickAct()
+    {
+        Actions.gameObject.SetActive(true);
+    }
+
+    public void clickChar()
+    {
+        Info.gameObject.SetActive(true);
+    }
+
+    public void clickStat()
+    {
+        Stat.gameObject.SetActive(true);
+    }
+
+    public void clickOpt()
+    {
+        Option.gameObject.SetActive(true);
+    }
+
+    public void clickSkip()
+    {
+
+    }
+}
